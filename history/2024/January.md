d03be752710c1538191161b4749e28745d9539dd
saman <samanf74@gmail.com>
2024-01-08 21:11:50 +0100

    update quote of the day for 08 January, 2024

--- history/2024/January.md
+++ history/2024/January.md
@@ -1,9 +1,7 @@
-### 📅 Monday, 08 January, 2024
-------
+## Monday, 08 January, 2024
 ### Events
-------
 - Capitol breach: On January 8, a violent mob stormed the United States Capitol in Washington, DC, resulting in the disruption of the certification of the 2020 presidential election results and the evacuation of lawmakers.
 - Assassination of Qasem Soleimani: On January 8, 2020, Qasem Soleimani, the commander of Iran's elite Quds Force, was killed in a targeted U.S. airstrike near Baghdad International Airport in Iraq.
-### Positive Qoute
-------
+### Quote of the day
 "Every day may not be good, but there is something good in every day." 🌞💫😊
+-----
--- main.py
+++ main.py
@@ -2,51 +2,182 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 from datetime import date
+import yaml
+import logging
+from typing import Optional 
 
-load_dotenv()
+def getConfig(path: Optional[str]) -> dict:
+  """
+  Get the config file.
 
-client = OpenAI()
-# reset session of client
+  Args:
+    path (Union[str, None]): The path to the config file. If None, the default path "config.yaml" will be used.
 
-# Define the prompt for the quote of the day
-prompt = "Give me a random positive quote of the day with emojies related to the quote based on event of the day" 
+  Returns:
+    dict: The contents of the config file as a dictionary.
+  """
+  if path is None:
+    path = "."
+  path += "/config.yaml"
+  with open(path, "r") as f:
+    return yaml.load(f, Loader=yaml.FullLoader)
 
-# send the prompt to the API
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": prompt}
-  ],
-  max_tokens=100
-)
+def getCurrentPath() -> str:
+    """
+    Get the current path
+    """
+    return os.path.dirname(os.path.realpath(__file__))
 
-print(completion.choices[0].message.content)
+def askFromGpt(prompt: str) -> str:
+    """
+    Ask the GPT-3 model to generate a response based on the prompt
+    """
+    # get the client session
+    client = OpenAI()
 
-template = f"""
-# Quote of the day
-### 📅 {date.today().strftime("%A, %d %B, %Y")}
-------
-{completion.choices[0].message.content}
-"""
+    # send the prompt to the API
+    completion = client.chat.completions.create(
+    model=config["gpt"]["model"],
+    temperature=config["gpt"]["temperature"],
+    max_tokens=config["gpt"]["max_tokens"],
+    messages=[
+        {"role": "user", "content": prompt}
+    ],
+    )
 
-# write the respone to README.md
-with open("README.md", "w") as f:
-    f.write(template)
+    return completion.choices[0].message.content
+
+if __name__ == "__main__":
+    load_dotenv()
+    logging.basicConfig(level=os.getenv("LOG_LEVEL", "WARNING"))
+    logging.info("Starting the program ...")
+    # log the current path
+    path = getCurrentPath()
+    logging.info(f"Current path: {getCurrentPath()}")
+    # load the config file
+    config = getConfig(path=path)
+    logging.info(f"Config file: \n{config}")
+
+    # get today date
+    today = date.today()
+
+    # Ask for the the 3 events that happened in this date
+    prompt = f"What are one event in usa and one event in iran in this date? (date={today.strftime('%d %B')}) without mention country but use bullet points"
+    logging.info(f"Prompt: {prompt}")
+    # ask the GPT model
+    events = askFromGpt(prompt)
+
+    # Ask for the quote of the day
+    prompt = f"Give me a random positive quote of the day with emojies related to the quote change every day"
+    logging.info(f"Prompt: {prompt}")
+    # ask the GPT model
+    quote = askFromGpt(prompt)
+
+    # create the history file of events
+    logging.info("Creating the history folder ...")
+    os.makedirs(f"{path}/history", exist_ok=True)
+    # create the folder of the current year
+    logging.info("Creating the folder of the current year ...")
+    os.makedirs(f"{path}/history/{today.year}", exist_ok=True)
+    # create the file of the current month
+    logging.info("Creating the file of the current month ...")
+    with open(f"{path}/history/{today.year}/{today.strftime('%B')}.md", "a") as f:
+        f.write(f"## {today.strftime('%A, %d %B, %Y')}\n")
+        f.write(f"### Events\n")
+        f.write(f"{events}\n")
+        f.write(f"### Quote of the day\n")
+        f.write(f"{quote}\n")
+        f.write(f"-----\n")
+    
+    # create the README.md file
+    logging.info("Creating the README.md file ...")
+    with open(f"{path}/README.md", "w") as f:
+        f.write(f"### 📅 {today.strftime('%A, %d %B, %Y')}\n")
+        f.write(f"------\n")
+
+        f.write(f"### Events\n")
+        f.write(f"------\n")
+        f.write(f"{events}\n")
+
+        f.write(f"### Positive Qoute\n")
+        f.write(f"------\n")
+        f.write(f"{quote}\n")
+
+    # commit the changes to the repo
+    logging.info("Pulling from Git ...")
+    os.system(f"git -C {path} pull" )
+    logging.info("Done")
+    
+    logging.info("Add and commiting ...")
+    os.system(f"git -C {path} add ." )
+    os.system(f"git -C {path} commit -m 'update quote of the day for {today.strftime('%d %B, %Y')}'" )
+    logging.info("Done")
+
+    logging.info("Pushing the new commit to git ...")
+    os.system(f"git -C {path} push" )
+    logging.info("Done")
+
+
+    
+
+
+
+    
+
+
+
+
+# load_dotenv()
+
+# # get the client session
+# client = OpenAI()
+
+# # Ask for the the 3 events that happened in this date 
+# prompt = "What are the 3 events that happened in this date?"
+# completion = client.chat.completions.create(
+
+
+# # Define the prompt for the quote of the day
+# prompt = "Give me a random positive quote of the day with emojies related to the quote based on event of the day" 
+
+# # send the prompt to the API
+# completion = client.chat.completions.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "user", "content": prompt}
+#   ],
+#   max_tokens=100
+# )
+
+# print(completion.choices[0].message.content)
+
+# template = f"""
+# # Quote of the day
+# ### 📅 {date.today().strftime("%A, %d %B, %Y")}
+# ------
+# {completion.choices[0].message.content}
+# """
+
+# # write the respone to README.md
+# with open("README.md", "w") as f:
+#     f.write(template)
+
+
 
 # commit the changes to the repo
-repo_path = os.getenv("REPO_PATH") # path to the repo
-print("Pulling from Git ...")
-os.system(f"git -C {repo_path} pull" )
-print("Done")
-print()
+# repo_path = os.getenv("REPO_PATH") # path to the repo
+# print("Pulling from Git ...")
+# os.system(f"git -C {repo_path} pull" )
+# print("Done")
+# print()
 
-print("Add and commiting ...")
-os.system(f"git -C {repo_path} add ." )
-os.system(f"git -C {repo_path} commit -m 'update quote of the day for {date.today().strftime('%d %B, %Y')}'" )
-print("Done")
-print()
+# print("Add and commiting ...")
+# os.system(f"git -C {repo_path} add ." )
+# os.system(f"git -C {repo_path} commit -m 'update quote of the day for {date.today().strftime('%d %B, %Y')}'" )
+# print("Done")
+# print()
 
-print("Pushing the new commit to git ...")
-os.system(f"git -C {repo_path} push" )
-print("Done")
-print()
+# print("Pushing the new commit to git ...")
+# os.system(f"git -C {repo_path} push" )
+# print("Done")
+# print()
